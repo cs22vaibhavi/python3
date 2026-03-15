3ad6137970a441f5dc57029fed237af5b34d4ac1
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-03-15 22:14:05 +0530

    Refactor simple calculator program
    Removed comments and adjusted print statements.

---
+++ simple.py
@@ -0,0 +1,17 @@
+print("Simple Calculator Program")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+total = num1 + num2 + num3
+
+average = total / 3
+print("First Number:", num1)
+print("Second Number:", num2)
+print("Third Number:", num3)
+
+print("Total =", total)
+print("Average =", average)
+
+print("Program Finished")

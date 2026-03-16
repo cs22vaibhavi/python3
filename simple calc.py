33a58bab39286fc969c6173e55961a8aea31ef24
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-03-16 21:09:52 +0530

    calculator
    Implement a simple calculator that performs addition, subtraction, multiplication, and division.

---
+++ simple calc.py
@@ -0,0 +1,16 @@
+print("Simple Calculator")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print("Addition =", sum)
+print("Subtraction =", sub)
+print("Multiplication =", mul)
+print("Division =", div)
+
+print("Program Finished")

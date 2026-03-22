2eb39f707519e6bcb2fa4776c807c982215d6914
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-03-22 23:52:37 +0530

    New5.py

---
+++ New5.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+print("Multiplication Table for", num)
+
+i = 1
+
+while i <= 10:
+    result = num * i
+    print(num, "x", i, "=", result)
+    i = i + 1
+
+print("Table completed!")
+print("Good job!")

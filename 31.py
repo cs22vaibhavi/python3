6b42bea0f68872b9982365a5b21dbe9868a3cfdf
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-03-31 23:23:13 +0530

    31.py

---
+++ 31.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+even = []
+odd = []
+
+for num in numbers:
+    if num % 2 == 0:
+        even.append(num)
+    else:
+        odd.append(num)
+
+print("Even numbers:", even)
+print("Odd numbers:", odd)
+print("Total numbers:", len(numbers))
